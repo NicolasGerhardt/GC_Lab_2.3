0a8f4feb280bade560c33c766369f5af7e209724
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-01-22 20:02:30 -0500

    added main loop an method for exiting the loop

--- GC_Lab_2.3/Program.cs
+++ GC_Lab_2.3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace GC_Lab_2._3
 {
@@ -6,7 +7,49 @@ namespace GC_Lab_2._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Welcome to the wonderful world of Regular Expressions!");
+            Console.WriteLine("You will pass a string to us, and we will tell you what patterns it matches");
+
+            do
+            {
+                Console.WriteLine("Boop!");
+            } while (checkForExitCondition());
+        }
+
+        static void validateAsName(string s)
+        {
+            var pattern = "^[A-Z]{0,1}[a-z]{1-30}$";
+
+            var matches = Regex.Match(s, pattern);
+
+            if (matches.Length > 0)
+            {
+                Console.WriteLine("This Matches a name");
+            }
+        }
+
+        static bool checkForExitCondition()
+        {
+            while (true)
+            {
+                Console.Write("\nDo you want to go around again? (y/n): ");
+                var keyPressed = Console.ReadKey();
+                Console.WriteLine();
+
+                if (keyPressed.KeyChar.ToString().ToLower() == "y")
+                {
+                    Console.WriteLine("\nSounds good! Lets Go!\n");
+                    return true;
+                }
+                else if (keyPressed.KeyChar.ToString().ToLower() == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("not a valid responce");
+                }
+            }
         }
     }
 }
